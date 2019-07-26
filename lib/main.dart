65dabaa3efb8f9efa87dcfe05d8b2ce2a1a5c390
Eralp Karaduman <eralp@eralpkaraduman.com>
2019-07-26 16:51:05 +0300

    Removed "other platform"

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,14 @@
 import 'package:event_dot_pizza/src/state/appState.dart';
 import 'package:event_dot_pizza/src/state/meetup_platform_session.dart';
-import 'package:event_dot_pizza/src/state/other_platform_session.dart';
 import 'package:flutter/material.dart';
 import 'package:event_dot_pizza/src/app.dart';
 import 'package:provider/provider.dart';
 
 void main() async {
   final meetupPlatformSession = await MeetupPlatformSession().loadFromPrefs();
-  final otherPlatformSession = await OtherPlatformSession().loadFromPrefs();
   runApp(MultiProvider(providers: [
     ChangeNotifierProvider(builder: (_) => meetupPlatformSession),
-    ChangeNotifierProvider(builder: (_) => otherPlatformSession),
-    ProxyProvider2<MeetupPlatformSession, OtherPlatformSession, AppState>(
-        builder: (context, meetup, other, _) => AppState([meetup, other]))
+    ProxyProvider<MeetupPlatformSession, AppState>(
+        builder: (context, meetupSession, _) => AppState([meetupSession]))
   ], child: App()));
 }
