ca0ef349f7c67fee508d5f3beec619361119db6c
Eralp Karaduman <eralp@eralpkaraduman.com>
2019-10-14 22:30:00 +0300

    Flutter autoconfig

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\src\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\eralp\Projects\event_dot_pizza"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_ROOT=/Users/eralp/Developer/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/eralp/Projects/event.pizza/event_dot_pizza"
+export "FLUTTER_TARGET=/Users/eralp/Projects/event.pizza/event_dot_pizza/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build\ios"
-export "FLUTTER_FRAMEWORK_DIR=C:\src\flutter\bin\cache\artifacts\engine\ios"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/eralp/Developer/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=5"
+export "TRACK_WIDGET_CREATION=true"
