ee7bac6fb629850b7de25f38e15d5927746cccc6
Eralp Karaduman <eralp@eralpkaraduman.com>
2019-12-19 16:29:06 +0200

    Changed meetup to authorize with callback uri

--- lib/pages/meetup_auth_page.dart
+++ lib/pages/meetup_auth_page.dart
@@ -1,87 +1,137 @@
 import 'package:event_dot_pizza/utils.dart';
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
 import 'package:provider/provider.dart';
 import '../providers/meetup_platform_session.dart';
 import '../platforms/meetup_platform_api.dart';
 
+import 'dart:async';
+import 'package:uni_links/uni_links.dart';
+
+import 'package:url_launcher/url_launcher.dart';
+import '../utils.dart';
+
 class MeetupAuthPage extends StatefulWidget {
   static const routeName = "meetupAuth";
   @override
-  _MeetupAuthPageState createState() => _MeetupAuthPageState();
+  _MeetupAuthPage createState() => _MeetupAuthPage();
 }
 
-class _MeetupAuthPageState extends State<MeetupAuthPage> {
-  bool _loading = false;
+class _MeetupAuthPage extends State<MeetupAuthPage>
+    with WidgetsBindingObserver {
+  bool _pending = false;
+  bool _failed = false;
+  StreamSubscription _callbackStream;
   MeetupPlatformSession _meetupPlatform;
-
-  Map<String, String> parseRedirectParams(String url) {
-    Map<String, String> paramsMap = Map();
-    if (url.startsWith(MeetupPlatformApi.REDIRECT_URI)) {
-      String query = url.split('#')[1];
-      List<String> params = query.split('&');
-      for (String params in params) {
-        List<String> pair = params.split('=');
-        paramsMap[pair[0]] = pair[1];
-      }
-    }
-    return paramsMap;
-  }
 
   @override
   void initState() {
     print('MeetupAuthPage:InitState');
-    _meetupPlatform =
-        Provider.of<MeetupPlatformSession>(context, listen: false);
-    setState(() => _loading = true);
+    _meetupPlatform = Provider.of<MeetupPlatformSession>(
+      context,
+      listen: false,
+    );
+    _callbackStream = getLinksStream().listen((String url) {
+      setState(() => _pending = false);
+      _handleUrlCallback(url);
+    }, onError: (err) {
+      print("Meetup:Auth:Failed:$err");
+      setState(() {
+        _pending = false;
+        _failed = true;
+      });
+    });
+    _launchAuthURL();
     super.initState();
-  }
-
-  NavigationDecision navigationDelegate(request) {
-    Map<String, String> params = parseRedirectParams(request.url);
-    if (params.containsKey('error')) {
-      print('errored');
-      Navigator.pop(context);
-      return NavigationDecision.prevent;
-    } else if (params.containsKey(MeetupPlatformApi.kACCESS_TOKEN)) {
-      print('contains token');
-      _meetupPlatform.connect(params[MeetupPlatformApi.kACCESS_TOKEN]);
-      Navigator.popUntil(
-        context,
-        ModalRoute.withName(Navigator.defaultRouteName),
-      );
-      return NavigationDecision.prevent;
-    } else {
-      return NavigationDecision.navigate;
-    }
+    WidgetsBinding.instance.addObserver(this);
   }
 
   @override
   Widget build(BuildContext context) {
     print('MeetupAuthPage:Build');
-    // TODO: Add UI For Back/Forward navigation (there's example in webview_flutter repo)
     return Scaffold(
       appBar: AppBar(
-        title: Text('Connect Meetup.Com'),
+        title: Text('Connect Meetup'),
       ),
       body: SafeArea(
-        child: Stack(
-          alignment: Alignment.center,
-          children: <Widget>[
-            WebView(
-              userAgent: getUserAgent(),
-              initialUrl: MeetupPlatformApi.authURI,
-              javascriptMode: JavascriptMode.unrestricted,
-              onPageFinished: (_) => setState(() => _loading = false),
-              navigationDelegate: navigationDelegate,
-            ),
-            Visibility(
-              visible: _loading,
-              child: CircularProgressIndicator(),
-            )
-          ],
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Visibility(
+                visible: _failed,
+                child: Text("Authorization Failed."),
+              ),
+              Visibility(
+                visible: _failed,
+                child: RaisedButton(
+                  child: Text("Try Again"),
+                  onPressed: _launchAuthURL,
+                ),
+              ),
+              Visibility(
+                visible: _pending,
+                child: CircularProgressIndicator(),
+              ),
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  _launchAuthURL() async {
+    setState(() {
+      _pending = true;
+      _failed = false;
+    });
+    await Future.delayed(const Duration(seconds: 1));
+    print(MeetupPlatformApi.authURI);
+    if (await canLaunch(MeetupPlatformApi.authURI)) {
+      await launch(
+        MeetupPlatformApi.authURI,
+        forceWebView: false,
+        forceSafariVC: false,
+      );
+    } else {
+      throw 'Could not launch ${MeetupPlatformApi.authURI}';
+    }
+  }
+
+  void _handleUrlCallback(String url) {
+    print('Handle Auth Callback Url: $url');
+    Map<String, String> params;
+    if (url.startsWith(MeetupPlatformApi.CALLBACK_URI)) {
+      params = parseRedirectParams(url);
+    }
+    if (params != null && params.containsKey('error')) {
+      this.setState(() => _failed = true);
+    } else if (params.containsKey(MeetupPlatformApi.kACCESS_TOKEN)) {
+      _meetupPlatform.connect(params[MeetupPlatformApi.kACCESS_TOKEN]);
+      Navigator.popUntil(
+        context,
+        ModalRoute.withName(Navigator.defaultRouteName),
+      );
+    } else {
+      this.setState(() => _failed = true);
+    }
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    Future.delayed(const Duration(seconds: 2), () {
+      if (state == AppLifecycleState.resumed && _pending) {
+        this.setState(() {
+          _pending = false;
+          _failed = true;
+        });
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    _callbackStream.cancel();
+    super.dispose();
   }
 }
