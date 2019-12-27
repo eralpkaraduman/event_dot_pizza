89c5a4887eb844e527d6eeccd0d44dcf6528be77
Eralp Karaduman <eralp@eralpkaraduman.com>
2019-12-27 14:15:32 +0200

    handle null at parseRedirectParams

--- test/utils_test.dart
+++ test/utils_test.dart
@@ -9,5 +9,7 @@ void main() {
     expect(params['access_token'], 'TOKEN');
     expect(params['token_type'], 'bearer');
     expect(params['expires_in'], '3600');
+    expect(parseRedirectParams(null), {});
+    expect(parseRedirectParams(''), {});
   });
 }
